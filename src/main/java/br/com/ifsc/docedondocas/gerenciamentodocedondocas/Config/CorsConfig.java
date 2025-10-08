d4cf86ec3b0eda2904e4bd7656ba0484dbb13e97
GabrieliBacca <110993557+GabrieliBacca@users.noreply.github.com>
2025-10-08 16:13:09 -0300

    CorsConfig

---
+++ src/main/java/br/com/ifsc/docedondocas/gerenciamentodocedondocas/Config/CorsConfig.java
@@ -0,0 +1,18 @@
+package br.com.ifsc.docedondocas.gerenciamentodocedondocas.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a configuração a todos os endpoints
+                .allowedOrigins("http://localhost:5173" ) // Permite requisições do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite o envio de cookies e credenciais
+    }
+}
